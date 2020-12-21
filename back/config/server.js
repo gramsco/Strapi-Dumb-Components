d9b66d196ba976ddf7991fd7dce895ca5163fe78
gramsco <antoningauth@gmail.com>
2020-12-21 02:34:02 +0100

    eppur si muove

---
+++ back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de4ec9d02f376039ce6639035672432c'),
+    },
+  },
+});
---
+++ back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ffe3ddc4-f49a-4d0a-bc3b-65913d55e89c'
+};
